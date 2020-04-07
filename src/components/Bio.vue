4fa427a8e490fe43fb1495b6df90faa7b5748b32
github username <github email address>
2020-04-06 21:14:05 -0400

    minor updates

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="bio">
     <h2>About</h2>
-    <p>As a lover of bands like Kerbera, Modern day escape, Jigsaw, Your Band, Beyond Unbroken, and The Sway, I got my beginnings in the music industry by interviewing bands.
+    <p>As a lover of bands like Kerbera, Modern Day Escape, Jigsaw, Symfinity, Beyond Unbroken, and The Sway, I got my beginnings in the music industry by interviewing bands.
       As time went on, I felt the need to pivot into becoming a booking agent which has been far more successful and rewarding for me. I am looking forward to working with newer and upcoming acts in the future.
     </p>
     <img alt="Kerbera" src="../assets/kerbera.jpg">
